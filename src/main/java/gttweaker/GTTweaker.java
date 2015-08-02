8447719043dcee576e524732a39e7457ee7561d1
DreamMaster <dream-master@gmx.net>
2015-08-02 23:57:07 +0200

    Fix the Blast furnace (no liquid Output) Change Name Spaces

--- src/main/java/gttweaker/GTTweaker.java
+++ src/main/java/gttweaker/GTTweaker.java
@@ -7,20 +7,20 @@ import gttweaker.mods.gregtech.*;
 import minetweaker.MineTweakerAPI;
 import gregtech.api.*;
 
-@Mod(modid = "GTTweaker", name = "GTTweaker", dependencies = "", version = "0.7.4")
+@Mod(modid = "GTTweaker", name = "GTTweaker", dependencies = "", version = "0.7.5")
 public class GTTweaker {
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
                 MineTweakerAPI.registerClass(Amplifabricator.class);
-                MineTweakerAPI.registerClass(AssemblerLiq.class);
+                MineTweakerAPI.registerClass(Assembler.class);
                 MineTweakerAPI.registerClass(Autoclave.class);
-                MineTweakerAPI.registerClass(BlastFurnaceLiq.class);
+                MineTweakerAPI.registerClass(Blastfurnace.class);
                 MineTweakerAPI.registerClass(Brewery.class);
-                MineTweakerAPI.registerClass(CentrifugeLiq.class);
-                MineTweakerAPI.registerClass(ChemicalBathLiq.class);
-                MineTweakerAPI.registerClass(ChemicalReactorLiq.class);
+                MineTweakerAPI.registerClass(Centrifuge.class);
+                MineTweakerAPI.registerClass(ChemicalBath.class);
+                MineTweakerAPI.registerClass(ChemicalReactor.class);
                 MineTweakerAPI.registerClass(Distillery.class);
-                MineTweakerAPI.registerClass(ElectrolyzerLiq.class);
+                MineTweakerAPI.registerClass(Electrolyzer.class);
                 MineTweakerAPI.registerClass(Fermenter.class);
                 MineTweakerAPI.registerClass(FluidCanner.class);
                 MineTweakerAPI.registerClass(FluidExtractor.class);
@@ -33,15 +33,15 @@ public class GTTweaker {
                 MineTweakerAPI.registerClass(PrecisionLaser.class);
                 MineTweakerAPI.registerClass(Printer.class);
                 MineTweakerAPI.registerClass(Pulverizer.class);
-                MineTweakerAPI.registerClass(SawLiq.class);
+                MineTweakerAPI.registerClass(CuttingSaw.class);
                 MineTweakerAPI.registerClass(Separator.class);
                 MineTweakerAPI.registerClass(Sifter.class);
                 MineTweakerAPI.registerClass(Slicer.class);
                 MineTweakerAPI.registerClass(Unpacker.class);
                 if(GregTech_API.VERSION>=508){
                 MineTweakerAPI.registerClass(ArcFurnace.class);
-                MineTweakerAPI.registerClass(DistillationTowerLiq.class);
-                MineTweakerAPI.registerClass(FusionReactorLiq.class);
+                MineTweakerAPI.registerClass(DistillationTower.class);
+                MineTweakerAPI.registerClass(FusionReactor.class);
                 MineTweakerAPI.registerClass(PlasmaArcFurnace.class);}
             }
 }
