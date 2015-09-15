755db4a5458e9651a55462888ab09dff55384db2
DreamMaster <dream-master@gmx.net>
2015-09-15 19:37:32 +0200

    Improve Mixer recipes are shorter if they have less than 4 inputs

--- src/main/java/gttweaker/mods/gregtech/Mixer.java
+++ src/main/java/gttweaker/mods/gregtech/Mixer.java
@@ -25,17 +25,18 @@ public class Mixer {
      *
      * @param output        recipe output
      * @param fluidOutput   primary fluidInput
-     * @param input1        primary input
-     * @param input2        secondary input
-     * @param input3        third input
-     * @param input4        fourth input
+     * @param input         input 1-4
      * @param fluidInput    primary fluidInput
      * @param durationTicks reaction time, in ticks
      * @param euPerTick     eu consumption per tick
      */
     @ZenMethod
-    public static void addRecipe(IItemStack output, ILiquidStack fluidOutput, IItemStack input1, IItemStack input2, IItemStack input3, IItemStack input4, ILiquidStack fluidInput, int durationTicks, int euPerTick) {
-        MineTweakerAPI.apply(new AddRecipeAction(output, fluidOutput, input1, input2, input3, input4, fluidInput, durationTicks, euPerTick));
+    public static void addRecipe(IItemStack output, ILiquidStack fluidOutput, IItemStack[] input, ILiquidStack fluidInput, int durationTicks, int euPerTick) {
+        if (input.length == 0) {
+            MineTweakerAPI.logError("Lathe recipe requires at least 1 input");
+        } else {
+            MineTweakerAPI.apply(new AddRecipeAction(output, fluidOutput, input, fluidInput, durationTicks, euPerTick));
+        }
     }
 
 // ######################
@@ -45,22 +46,16 @@ public class Mixer {
 
         private final IItemStack output;
         private final ILiquidStack fluidOutput;
-        private final IItemStack input1;
-        private final IItemStack input2;
-        private final IItemStack input3;
-        private final IItemStack input4;
+        private final IItemStack[] input;
         private final ILiquidStack fluidInput;
         private final int duration;
         private final int euPerTick;
 
-        public AddRecipeAction(IItemStack output, ILiquidStack fluidOutput, IItemStack input1, IItemStack input2, IItemStack input3, IItemStack input4, ILiquidStack fluidInput, int duration, int euPerTick) {
+        public AddRecipeAction(IItemStack output, ILiquidStack fluidOutput, IItemStack[] input, ILiquidStack fluidInput, int duration, int euPerTick) {
 
             this.output = output;
             this.fluidOutput = fluidOutput;
-            this.input1 = input1;
-            this.input2 = input2;
-            this.input3 = input3;
-            this.input4 = input4;
+            this.input = input;
             this.fluidInput = fluidInput;
             this.duration = duration;
             this.euPerTick = euPerTick;
@@ -69,10 +64,10 @@ public class Mixer {
         @Override
         public void apply() {
             RA.addMixerRecipe(
-                    MineTweakerMC.getItemStack(input1),
-                    MineTweakerMC.getItemStack(input2),
-                    MineTweakerMC.getItemStack(input3),
-                    MineTweakerMC.getItemStack(input4),
+                    MineTweakerMC.getItemStack(input[0]),
+                    input.length > 1 ? MineTweakerMC.getItemStack(input[1]) : null,
+                    input.length > 2 ? MineTweakerMC.getItemStack(input[2]) : null,
+                    input.length > 3 ? MineTweakerMC.getItemStack(input[3]) : null,
                     MineTweakerMC.getLiquidStack(fluidInput),
                     MineTweakerMC.getLiquidStack(fluidOutput),
                     MineTweakerMC.getItemStack(output),
@@ -95,10 +90,7 @@ public class Mixer {
             int hash = 6;
             hash = 67 * hash + (this.output != null ? this.output.hashCode() : 0);
             hash = 67 * hash + (this.fluidOutput != null ? this.fluidOutput.hashCode() : 0);
-            hash = 67 * hash + (this.input1 != null ? this.input1.hashCode() : 0);
-            hash = 67 * hash + (this.input2 != null ? this.input2.hashCode() : 0);
-            hash = 67 * hash + (this.input3 != null ? this.input3.hashCode() : 0);
-            hash = 67 * hash + (this.input4 != null ? this.input4.hashCode() : 0);
+            hash = 67 * hash + (this.input != null ? this.input.hashCode() : 0);
             hash = 67 * hash + (this.fluidInput != null ? this.fluidInput.hashCode() : 0);
             hash = 67 * hash + this.duration;
             hash = 67 * hash + this.euPerTick;
@@ -120,16 +112,7 @@ public class Mixer {
             if (this.fluidOutput != other.fluidOutput && (this.fluidOutput == null || !this.fluidOutput.equals(other.fluidOutput))) {
                 return false;
             }
-            if (this.input1 != other.input1 && (this.input1 == null || !this.input1.equals(other.input1))) {
-                return false;
-            }
-            if (this.input2 != other.input2 && (this.input2 == null || !this.input2.equals(other.input2))) {
-                return false;
-            }
-            if (this.input3 != other.input3 && (this.input3 == null || !this.input3.equals(other.input3))) {
-                return false;
-            }
-            if (this.input4 != other.input4 && (this.input4 == null || !this.input4.equals(other.input4))) {
+            if (this.input != other.input && (this.input == null || !this.input.equals(other.input))) {
                 return false;
             }
             if (this.fluidInput != other.fluidInput && (this.fluidInput == null || !this.fluidInput.equals(other.fluidInput))) {
