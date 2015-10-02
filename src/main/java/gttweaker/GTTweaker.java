d71ef1a3d9f3c4eb60ae05b96e2f551a20e24d58
Martin <dream-master@gmx.net>
2015-10-02 16:41:13 +0200

    versions bump

--- src/main/java/gttweaker/GTTweaker.java
+++ src/main/java/gttweaker/GTTweaker.java
@@ -7,7 +7,7 @@ import gttweaker.mods.gregtech.*;
 import minetweaker.MineTweakerAPI;
 import gregtech.api.*;
 
-@Mod(modid = "GTTweaker", name = "GTTweaker", dependencies = "", version = "1.0.8")
+@Mod(modid = "GTTweaker", name = "GTTweaker", dependencies = "", version = "1.0.9")
 public class GTTweaker {
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
