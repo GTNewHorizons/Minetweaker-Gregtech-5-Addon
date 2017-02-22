ae3090ce3d1179503ee0c5cbfa0c004fc976e1e7
John <Techlone@users.noreply.github.com>
2017-02-22 15:55:46 +0500

    Revert "1.10"

--- src/main/java/gttweaker/mods/gregtech/machines/Amplifabricator.java
+++ src/main/java/gttweaker/mods/gregtech/machines/Amplifabricator.java
@@ -1,10 +1,9 @@
 package gttweaker.mods.gregtech.machines;
 
+import gttweaker.mods.gregtech.AddMultipleRecipeAction;
 import minetweaker.MineTweakerAPI;
-import minetweaker.OneWayAction;
 import minetweaker.annotations.ModOnly;
-import minetweaker.api.item.IItemStack;
-import minetweaker.api.minecraft.MineTweakerMC;
+import minetweaker.api.item.IIngredient;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -22,79 +21,16 @@ public class Amplifabricator {
     /**
      * Adds a Amplifabricator recipe.
      *
-     * @param input        primary Input
-     * @param duration     reaction time, in ticks
-     * @param amount       amount in mb of Liquid UU Output
-     *
+     * @param input    primary Input
+     * @param duration reaction time, in ticks
      */
     @ZenMethod
-    public static void addRecipe(IItemStack input,  int duration, int amount) {
-        MineTweakerAPI.apply(new AddRecipeAction(input, duration, amount));
-    }
-
-    // ######################
-    // ### Action classes ###
-    // ######################
-
-    private static class AddRecipeAction extends OneWayAction {
-
-        private final IItemStack input;
-        private final int duration;
-        private final int amount;
-
-        public AddRecipeAction(IItemStack input, int duration, int amount) {
-
-            this.input = input;
-            this.duration = duration;
-            this.amount = amount;
-        }
-
-        @Override
-        public void apply() {
-            RA.addAmplifier(
-                    MineTweakerMC.getItemStack(input),
-                    duration,
-                    amount);
-        }
-
-        @Override
-        public String describe() {
-            return "Adding Amplifabricator recipe for " + input ;
-        }
-
-        @Override
-        public Object getOverrideKey() {
-            return null;
-        }
-
-        @Override
-        public int hashCode() {
-            int hash = 1;
-            hash = 12 * hash + (this.input != null ? this.input.hashCode() : 0);
-            hash = 12 * hash + this.duration;
-            hash = 12 * hash + this.amount;
-            return hash;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
+    public static void addRecipe(IIngredient input, int duration, int amount) {
+        MineTweakerAPI.apply(new AddMultipleRecipeAction("Adding Amplifabricator recipe for " + input, input, duration, amount) {
+            @Override
+            protected void applySingleRecipe(ArgIterator i) {
+                RA.addAmplifier(i.nextItem(), i.nextInt(), i.nextInt());
             }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final AddRecipeAction other = (AddRecipeAction) obj;
-            if (this.input != other.input && (this.input == null || !this.input.equals(other.input))) {
-                return false;
-            }
-            if (this.duration != other.duration) {
-                return false;
-            }
-            if (this.amount != other.amount) {
-                return false;
-            }
-            return true;
-        }
+        });
     }
 }
--- src/main/java/gttweaker/mods/gregtech/machines/ChemicalReactor.java
+++ src/main/java/gttweaker/mods/gregtech/machines/ChemicalReactor.java
@@ -1,11 +1,11 @@
 package gttweaker.mods.gregtech.machines;
 
+import gttweaker.mods.gregtech.AddMultipleRecipeAction;
 import minetweaker.MineTweakerAPI;
-import minetweaker.OneWayAction;
 import minetweaker.annotations.ModOnly;
+import minetweaker.api.item.IIngredient;
 import minetweaker.api.item.IItemStack;
 import minetweaker.api.liquid.ILiquidStack;
-import minetweaker.api.minecraft.MineTweakerMC;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -24,174 +24,30 @@ public class ChemicalReactor {
      * Adds a Chemical Reactor recipe.
      *
      * @param output        recipe output
-     * @param fluidOutput1  primary fluidInput
+     * @param fluidOutput   primary fluidInput
      * @param input1        primary input
      * @param input2        secondary input
-     * @param fluidInput1   primary fluidInput
+     * @param fluidInput    primary fluidInput
      * @param durationTicks reaction time, in ticks
      * @param euPerTick     eu consumption per tick
      */
     @ZenMethod
-
-  public static void addRecipe(IItemStack output, ILiquidStack fluidOutput1, IItemStack input1, IItemStack input2, ILiquidStack fluidInput1, int durationTicks) {
-        MineTweakerAPI.apply(new AddFluidRecipeAction(output, fluidOutput1, input1, input2, fluidInput1, durationTicks));
+    public static void addRecipe(IItemStack output, ILiquidStack fluidOutput, IIngredient input1, IIngredient input2, ILiquidStack fluidInput, int durationTicks, int euPerTick) {
+        MineTweakerAPI.apply(new AddMultipleRecipeAction("Adding Chemical Reactor recipe for " + output, input1, input2, fluidInput, fluidOutput, output, durationTicks, euPerTick) {
+            @Override
+            protected void applySingleRecipe(ArgIterator i) {
+                RA.addChemicalRecipe(i.nextItem(), i.nextItem(), i.nextFluid(), i.nextFluid(), i.nextItem(), i.nextInt(), i.nextInt());
+            }
+        });
     }
 
     @ZenMethod
-    public static void addRecipe(IItemStack output, IItemStack input1, IItemStack input2,  int durationTicks) {
-        MineTweakerAPI.apply(new AddRecipeAction(output, input1, input2, durationTicks));
+    public static void addRecipe(IItemStack output, ILiquidStack fluidOutput, IIngredient input1, IIngredient input2, ILiquidStack fluidInput, int durationTicks) {
+        addRecipe(output, fluidOutput, input1, input2, fluidInput, durationTicks, 30);
     }
 
-// ######################
-// ### Action classes ###
-// ######################
-
-    private static class AddRecipeAction extends OneWayAction {
-        private final IItemStack output;
-        private final IItemStack input1;
-        private final IItemStack input2;
-        private final int duration;
-
-        public AddRecipeAction(IItemStack output, IItemStack input1, IItemStack input2, int duration) {
-            this.output = output;
-            this.input1 = input1;
-            this.input2 = input2;
-            this.duration = duration;
-        }
-
-        @Override
-        public void apply() {
-            RA.addChemicalRecipe(
-                    MineTweakerMC.getItemStack(input1),
-                    MineTweakerMC.getItemStack(input2),
-                    MineTweakerMC.getItemStack(output),
-                    duration);
-        }
-
-        @Override
-        public String describe() {
-            return "Adding Chemical Reactor recipe for " + output;
-        }
-
-        @Override
-        public Object getOverrideKey() {
-            return null;
-        }
-
-        @Override
-        public int hashCode() {
-            int hash = 7;
-            hash = 11 * hash + (this.output != null ? this.output.hashCode() : 0);
-            hash = 11 * hash + (this.input1 != null ? this.input1.hashCode() : 0);
-            hash = 11 * hash + (this.input2 != null ? this.input2.hashCode() : 0);
-            hash = 11 * hash + this.duration;
-            return hash;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final AddRecipeAction other = (AddRecipeAction) obj;
-            if (this.output != other.output && (this.output == null || !this.output.equals(other.output))) {
-                return false;
-            }
-            if (this.input1 != other.input1 && (this.input1 == null || !this.input1.equals(other.input1))) {
-                return false;
-            }
-            if (this.input2 != other.input2 && (this.input2 == null || !this.input2.equals(other.input2))) {
-                return false;
-            }
-            if (this.duration != other.duration) {
-                return false;
-            }
-            return true;
-        }
-    }
-
-    private static class AddFluidRecipeAction extends OneWayAction {
-        private final IItemStack output;
-        private final ILiquidStack fluidOutput1;
-        private final IItemStack input1;
-        private final IItemStack input2;
-        private final ILiquidStack fluidInput1;
-        private final int duration;
-
-        public AddFluidRecipeAction(IItemStack output, ILiquidStack fluidOutput1, IItemStack input1, IItemStack input2, ILiquidStack fluidInput1, int duration) {
-            this.output = output;
-            this.fluidOutput1 = fluidOutput1;
-            this.input1 = input1;
-            this.input2 = input2;
-            this.fluidInput1 = fluidInput1;
-            this.duration = duration;
-        }
-
-        @Override
-        public void apply() {
-            RA.addChemicalRecipe(
-                    MineTweakerMC.getItemStack(input1),
-                    MineTweakerMC.getItemStack(input2),
-                    MineTweakerMC.getLiquidStack(fluidInput1),
-                    MineTweakerMC.getLiquidStack(fluidOutput1),
-                    MineTweakerMC.getItemStack(output),
-                    duration);
-        }
-
-        @Override
-        public String describe() {
-            return "Adding Chemical Reactor recipe with Liquid support for " + output;
-        }
-
-        @Override
-        public Object getOverrideKey() {
-            return null;
-        }
-
-        @Override
-        public int hashCode() {
-            int hash = 7;
-            hash = 12 * hash + (this.output != null ? this.output.hashCode() : 0);
-            hash = 12 * hash + (this.fluidOutput1 != null ? this.fluidOutput1.hashCode() : 0);
-            hash = 12 * hash + (this.input1 != null ? this.input1.hashCode() : 0);
-            hash = 12 * hash + (this.input2 != null ? this.input2.hashCode() : 0);
-            hash = 12 * hash + (this.fluidInput1 != null ? this.fluidInput1.hashCode() : 0);
-            hash = 12 * hash + this.duration;
-            return hash;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final AddFluidRecipeAction other = (AddFluidRecipeAction) obj;
-            if (this.output != other.output && (this.output == null || !this.output.equals(other.output))) {
-                return false;
-            }
-            if (this.fluidOutput1 != other.fluidOutput1 && (this.fluidOutput1 == null || !this.fluidOutput1.equals(other.fluidOutput1))) {
-                return false;
-            }
-            if (this.input1 != other.input1 && (this.input1 == null || !this.input1.equals(other.input1))) {
-                return false;
-            }
-            if (this.input2 != other.input2 && (this.input2 == null || !this.input2.equals(other.input2))) {
-                return false;
-            }
-            if (this.fluidInput1 != other.fluidInput1 && (this.fluidInput1 == null || !this.fluidInput1.equals(other.fluidInput1))) {
-                return false;
-            }
-            if (this.duration != other.duration) {
-                return false;
-            }
-            return true;
-        }
-
+    @ZenMethod
+    public static void addRecipe(IItemStack output, IIngredient input1, IIngredient input2, int durationTicks) {
+        addRecipe(output, null, input1, input2, null, durationTicks);
     }
 }
