1b025594ab0a46406d4a39542f7ba0d1d521fd7f
Pilad <piladt@gmail.com>
2020-01-07 23:35:25 +0600

    Add files via upload

---
+++ src/main/java/gttweaker/mods/gtpp/machines/MultiblockElectrolyzer.java
@@ -0,0 +1,36 @@
+package gttweaker.mods.gtpp.machines;
+
+import gtPlusPlus.core.lib.CORE;
+import gttweaker.mods.AddMultipleRecipeAction;
+import minetweaker.MineTweakerAPI;
+import minetweaker.annotations.ModOnly;
+import minetweaker.api.item.IIngredient;
+import minetweaker.api.liquid.ILiquidStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+import minetweaker.api.item.IItemStack;
+
+@ZenClass("mods.gtpp.MultiblockElectrolyzer")
+@ModOnly(CORE.MODID)
+/*
+ItemStack[] aInputs, 
+FluidStack[] aFluidInputs, 
+FluidStack[] aFluidOutputs, 
+ItemStack[] aOutputs, 
+int[] aChances, 
+int aDuration, 
+int aEUtick, 
+int aSpecial
+*/	
+	public class MultiblockElectrolyzer {
+    @ZenMethod
+    public static void addRecipe(IItemStack[] inputs, ILiquidStack fluidInput, ILiquidStack fluidOutput, IItemStack[] outputs, int[] chances, int durationTicks, int euPerTick, int Special) {
+        MineTweakerAPI.apply(new AddMultipleRecipeAction("Adding Multiblock Electrolyzer recipe for " + inputs, outputs, fluidOutput, fluidInput, inputs, chances, durationTicks, euPerTick, Special) {
+            @Override
+            protected void applySingleRecipe(ArgIterator i) {
+                CORE.RA.addMultiblockElectrolyzerRecipe(i.nextItemArr(), i.nextFluidArr(), i.nextFluidArr(), i.nextItemArr(), 
+				i.nextIntArr(), i.nextInt(), i.nextInt(), i.nextInt());
+            }
+        });
+    }
+}
